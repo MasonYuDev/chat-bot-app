cf5a3fbc1898c183f7618210e87f73a1981570fb
Mason <mason@aretemedia.io>
2023-11-27 01:47:14 -0600

    revamped streamlit

--- app.py
+++ app.py
@@ -6,32 +6,29 @@ from construct_index import construct_index
 def main():
     port = int(os.environ.get("PORT", 8501))
 
-
     st.title("Your Chatbot")
     st.header("Use Your Chatbot")
     
-    st.text_input("Enter GitHub Repository URL:", key = "repo_url")
-    st.text_input("Enter OpenAI API Key:", key = "api_key")
-    
-    if st.button("Submit"):
+    with st.form(key='user_input_form'):
+        st.session_state.repo_url = st.text_input("Enter GitHub Repository URL:")
+        st.session_state.api_key = st.text_input("Enter OpenAI API Key:", type='password')
+        st.session_state.submitted = st.form_submit_button('Submit')
+
+    if st.session_state.submitted:
         if not st.session_state.repo_url or not st.session_state.api_key:
             st.error("Please enter a valid GitHub Repository URL and OpenAI API Key.")
-            return
+        else:
+            st.success("Submission successful. Repository cloning and indexing will begin.")
+            repo_name = os.path.basename(st.session_state.repo_url.rstrip('/'))
+            repo_folder = os.path.join('/tmp/content', repo_name)
+            os.makedirs(repo_folder, exist_ok=True)
+ 
+            clone_cmd = f"git clone {st.session_state.repo_url} {repo_folder}"
+            os.system(clone_cmd)
 
-    # Create a folder with the repository name
-    repo_name = os.path.basename(st.session_state.repo_url.rstrip('/'))
-    repo_folder = os.path.join('/tmp/content', repo_name)
-    os.makedirs(repo_folder, exist_ok=True)
-
-    # Clone the repository
-    clone_cmd = f"git clone {st.session_state.repo_url} {repo_folder}"
-    os.system(clone_cmd)
-
-    # Use the correct path to the cloned repository
-    directory_path = repo_folder
-    st.session_state["query_engine"] = construct_index(directory_path, st.session_state.api_key)
-
-    st.success("Repository cloned and indexed successfully!")
+            directory_path = repo_folder
+            st.session_state["query_engine"] = construct_index(directory_path, st.session_state.api_key)
+            st.success("Repository cloned and indexed successfully!")
 
     query = st.chat_input("What do you want to know?")
     if query and st.session_state.query_engine:
@@ -39,7 +36,6 @@ def main():
         if query.lower() == "quit":
             st.stop()
         else:
-            # Query the engine and display the result
             response = st.session_state.query_engine.query(query)
             st.write(f"Mason says: {response.response}")
             
