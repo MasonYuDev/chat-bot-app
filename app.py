239b3d8d15dd232c3bf98aa772565cb246142141
Mason <mason@aretemedia.io>
2023-11-30 22:41:15 -0600

    revamped with json

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import os
 from construct_index import construct_index
+import json
+import tempfile
 
 
 def main():
@@ -12,27 +14,56 @@ def main():
              and your OpenAI API key. After that, you can ask it to write application questions for you as long as the content
              you uploaded is relevant to your query.''')
     
+    content_source = st.radio("Choose content source:", ["GitHub Repo", "JSON File"])
+
     with st.form(key='user_input_form'):
-        st.session_state.repo_url = st.text_input("Enter GitHub Repository URL:")
+        if content_source == "GitHub Repo":
+            st.session_state.repo_url = st.text_input("Enter GitHub Repository URL:")
+        elif content_source == "JSON File":
+            uploaded_file = st.file_uploader("Upload JSON File", type=["json"])
         st.session_state.api_key = st.text_input("Enter OpenAI API Key:", type='password')
         st.session_state.submitted = st.form_submit_button('Submit')
 
     if st.session_state.submitted:
-        if not st.session_state.repo_url or not st.session_state.api_key:
-            st.error("Please enter a valid GitHub Repository URL and OpenAI API Key.")
-        else:
-            st.success("Submission successful. Repository cloning and indexing will begin.")
-            repo_name = os.path.basename(st.session_state.repo_url.rstrip('/'))
-            repo_folder = os.path.join('/tmp/content', repo_name)
-            os.makedirs(repo_folder, exist_ok=True)
- 
-            clone_cmd = f"git clone {st.session_state.repo_url} {repo_folder}"
-            os.system(clone_cmd)
+        if content_source == "GitHub Repo":
+            if not st.session_state.repo_url or not st.session_state.api_key:
+                st.error("Please enter a valid GitHub Repository URL and OpenAI API Key.")
+            else:
+                st.success("Submission successful. Repository cloning and indexing will begin.")
+                repo_name = os.path.basename(st.session_state.repo_url.rstrip('/'))
+                repo_folder = os.path.join('/tmp/content', repo_name)
+                os.makedirs(repo_folder, exist_ok=True)
 
-            directory_path = repo_folder
-            st.session_state["query_engine"] = construct_index(directory_path, st.session_state.api_key)
-            st.success("Repository cloned and indexed successfully!")
+                clone_cmd = f"git clone {st.session_state.repo_url} {repo_folder}"
+                os.system(clone_cmd)
 
+                directory_path = repo_folder
+                st.session_state["query_engine"] = construct_index(content_source, directory_path, st.session_state.api_key)
+                st.success("Repository cloned and indexed successfully!")
+
+        elif content_source == "JSON File":
+            if uploaded_file is not None:
+                # Read the file content
+                temp_dir = tempfile.mkdtemp()
+
+            # Read the file content
+                json_data = uploaded_file.read().decode('utf-8')
+                data = json.loads(json_data)
+
+            # Save each JSON entry as a separate file in the temporary directory
+                for idx, entry in enumerate(data):
+                    entry_filename = f"json_entry_{idx}.json"
+                    entry_filepath = os.path.join(temp_dir, entry_filename)
+                    with open(entry_filepath, 'w') as entry_file:
+                        json.dump(entry, entry_file)
+                print(temp_dir)
+                st.session_state["query_engine"] = construct_index(content_source, temp_dir, st.session_state.api_key)
+                st.success("JSON File uploaded and indexed successfully!")
+
+            else:
+                st.warning("Please upload a valid JSON file.")
+            
+    
     query = st.chat_input("What do you want to know?")
     if query and st.session_state.query_engine:
         st.write(query)
--- construct_index.py
+++ construct_index.py
@@ -1,12 +1,13 @@
-from llama_index import SimpleDirectoryReader, ServiceContext, GPTListIndex, readers, VectorStoreIndex, LLMPredictor, PromptHelper, StorageContext, load_index_from_storage
+from llama_index import SimpleDirectoryReader, ServiceContext, VectorStoreIndex, LLMPredictor, PromptHelper
 from llama_index.callbacks import CallbackManager, LlamaDebugHandler
 from langchain.chat_models import ChatOpenAI
-import streamlit as st
-import sys
+from llama_index.node_parser.interface import MetadataMode
+
 import os
+import uuid
+import json
 
-
-def construct_index(directory_path, openai_key):
+def construct_index(content_source, content_data, openai_key):
     # set maximum input size
     max_input_size = 4096
     # set number of output tokens
@@ -28,9 +29,68 @@ def construct_index(directory_path, openai_key):
 
     service_context = ServiceContext.from_defaults(callback_manager=callback_manager, llm_predictor=llm_predictor)
 
-    documents = SimpleDirectoryReader(directory_path).load_data()
+    if content_source == "GitHub Repo":
+        # Load content from GitHub
+        documents = SimpleDirectoryReader(content_data).load_data()
+        print(documents)
+    elif content_source == "JSON File":
+        documents = SimpleDirectoryReader(content_data).load_data()
+
+        # Extract relevant information, modify as needed based on your JSON structure
+        
+    else:
+        raise ValueError("Invalid content source. Supported sources: 'GitHub', 'JSON'")
     index = VectorStoreIndex.from_documents(documents, service_context=service_context)
+    print(index)
     query_engine = index.as_query_engine()
 
     return query_engine
 
+
+
+class JSONDocument:
+    def __init__(self, title, url, html):
+        self.id_ = str(uuid.uuid4())  # Generate a unique ID
+        self.embedding = None
+        self.metadata = {
+            'file_path': None,
+            'file_name': None,
+            'file_type': None,
+            'file_size': None,
+            'creation_date': None,
+            'last_modified_date': None,
+            'last_accessed_date': None
+        }
+        self.excluded_embed_metadata_keys = [
+            'file_name', 'file_type', 'file_size', 'creation_date',
+            'last_modified_date', 'last_accessed_date'
+        ]
+        self.excluded_llm_metadata_keys = self.excluded_embed_metadata_keys
+        self.relationships = {}
+        self.hash = None
+        self.text = f"{title}\n{url}\n{html}"
+
+    def get_doc_id(self):
+        return self.id_
+
+    def get_metadata_str(self, mode):
+        # Convert metadata dictionary to a JSON-formatted string
+        metadata_to_str = {
+            '2': lambda: json.dumps(self.metadata, default=str),
+            '3': lambda: json.dumps(self.metadata, default=str)
+        }
+        return metadata_to_str[mode]()
+    
+    def get_content(self, metadata_mode):
+        # Return text content
+        return self.text
+    
+def transform_json_to_documents(json_data):
+    return [
+        JSONDocument(
+            title=entry["title"],
+            url=entry["url"],
+            html=entry["html"]
+        )
+        for entry in json_data
+        ]
